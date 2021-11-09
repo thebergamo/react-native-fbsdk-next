27ee1c6dee79629dc0e9954b9482f56fb71f7c65
Mike Hardy <github@mikehardy.net>
2021-11-09 16:09:37 -0500

    docs(example): improve contribution / development docs, update refresh-example.sh
    This includes all the current configuration items noted in app startup logs / native
    integration guides
    
    this improves the developer experience, all things noted or needed while
    I was working with the example to update the android SDK

--- RNFBSDKExample/README.md
+++ RNFBSDKExample/README.md
@@ -2,7 +2,7 @@
 
 This example is extremely basic but hopefully still useful.
 
-You run it by cloning the main repo, and running `yarn` then `yarn example:android` or `yarn example:ios`
+You run it by cloning the main repo, and running `yarn example:install` then `yarn example:start`, and finally `yarn example:android` or `yarn example:ios`
 
 The example currently validates these things:
 
@@ -17,7 +17,7 @@ The example currently validates these things:
 
 ## Known Issues
 
-- The App ID in use for the example is not configured, and it is problematic to configure one for the internet at large. Replace the App ID in `ios/RNFBSDKExample/Info.plist` and `android/src/main/res/values/strings.xml` with your App ID
+- The Facebook App ID in use for the example is not configured, and it is problematic to configure one for the internet at large. Replace the App ID in `ios/RNFBSDKExample/Info.plist` and `android/src/main/res/values/strings.xml` with your App ID
 - It does not demonstrate anything else, contributions welcome
 
 ## How to contribute to the example
@@ -25,3 +25,15 @@ The example currently validates these things:
 - First, note that it is automatically constructed. The only file that is preserved when `refresh-example.sh` is run is `App.js`
 - Make all UI changes in `App.js`
 - Make all config changes (`Info.plist` or `.java` / `.m` file changes via sed edits in `refresh-example.sh`. No, that's not the easiest thing to do. But unless you commit to maintaining the project and updating the example app yourself for a few years, it is still the most efficient way)
+
+## Using the example to test changes to the core module
+
+If you are making changes to the core module and using the example for testing, you must understand the example references the module code via a github reference.
+
+The example will contain stale files unless you clear the lockfiles/modules, copy in the module code and reinstall. There are three commands which are useful for development that help you manage this. You may run all of these from the main directory:
+
+- `yarn example:clean` - this removes node_modules and the yarn and pod lockfiles
+- `yarn example:install` - this just runs `yarn` and `pod install` for you
+- `yarn example:devcopy` - this runs `yarn prepare` to build the core module, then copies the modules files in to the example
+
+A typical workflow will involve you making changes in the core src / android / ios folders, then running `yarn example:devcopy` and - if you changed something in package.json or the podspec then `yarn example:install` repeatedly, while perhaps rebuilding the the native code in `RNFBSDKExample` as if it were a normal project (that is, `yarn android` or `yarn ios` to see the fresh native changes)
--- refresh-example.sh
+++ refresh-example.sh
@@ -27,22 +27,100 @@ npx react-native init RNFBSDKExample
 pushd RNFBSDKExample
 yarn add github:thebergamo/react-native-fbsdk-next
 
-# Patch in our Facebook App ID - note this one does not work, but it *does* initialize and prove things to a point.
-# You may specify your own in these files to test your app.
-sed -i -e 's/<\/resources>/    <string name="facebook_app_id">950110338385665<\/string>\n<\/resources>/' android/app/src/main/res/values/strings.xml
-rm -f android/app/src/main/res/values/strings.xml??
-sed -i -e 's/^<\/dict>/  <key>FacebookAppID<\/key>\n  <string>950110338385665<\/string>\n<\/dict>/' ios/RNFBSDKExample/Info.plist
-rm -f ios/RNFBSDKExample/Info.plist??
+#########################################################################
+#
+# General compile fixes and tweaks, not react-native-fbsdk-next specific
+#
 
-# Patch the AndroidManifest directly to add our SDK initialization bit
-sed -i -e 's/<\/application>/  <meta-data android:name="com.facebook.sdk.ApplicationId" android:value="@string\/facebook_app_id"\/>\\\n    <\/application>/' android/app/src/main/AndroidManifest.xml
+# We want to test Android12 compatibility, so let's bump up our compile and target versions on android:
+sed -i -e $'s/compileSdkVersion = 30/compileSdkVersion = 31/' android/build.gradle
+sed -i -e $'s/targetSdkVersion = 30/targetSdkVersion = 31/' android/build.gradle
+rm -f android/build.gradle??
+# Android 12 does require a tweak to the stock template AndroidManifests, add exported to all activities for compliance
+sed -i -e $'s/android:launchMode/android:exported="true"\\\n        android:launchMode/' android/app/src/main/AndroidManifest.xml
 rm -f android/app/src/main/AndroidManifest.xml??
+sed -i -e $'s/DevSettingsActivity"/DevSettingsActivity" android:exported="true"/' android/app/src/debug/AndroidManifest.xml
+rm -f android/app/src/debug/AndroidManifest.xml??
 
-# Mixing Swift and Objective-C in a project may be problematic and react-native is Objective-C mostly while the SDK is Swift.
-# Workaround:  https://github.com/facebookarchive/react-native-fbsdk/issues/755#issuecomment-787488994
-sed -i -e $'s/react_native_post_install(installer)/react_native_post_install(installer)\\\n    \\\n    # Mixing Swift and Objective-C in a react-native project may be problematic.\\\n    # Workaround:  https:\/\/github.com\/facebookarchive\/react-native-fbsdk\/issues\/755#issuecomment-787488994\\\n    installer.aggregate_targets.first.user_project.native_targets.each do |target|\\\n      target.build_configurations.each do |config|\\\n        config.build_settings[\'LIBRARY_SEARCH_PATHS\'] = [\'$(inherited)\', \'$(SDKROOT)\/usr\/lib\/swift\']\\\n      end\\\n    end/' ios/Podfile
+# Apple builds in general have a problem with architectures on Apple Silicon and Intel, and doing some exclusions should help
+sed -i -e $'s/react_native_post_install(installer)/react_native_post_install(installer)\\\n\\\n    installer.aggregate_targets.each do |aggregate_target|\\\n      aggregate_target.user_project.native_targets.each do |target|\\\n        target.build_configurations.each do |config|\\\n          config.build_settings[\'ONLY_ACTIVE_ARCH\'] = \'YES\'\\\n          config.build_settings[\'EXCLUDED_ARCHS\'] = \'i386\'\\\n        end\\\n      end\\\n      aggregate_target.user_project.save\\\n    end/' ios/Podfile
 rm -f ios/Podfile??
 
+# This is just a speed optimization, very optional, but asks xcodebuild to use clang and clang++ without the fully-qualified path
+# That means that you can then make a symlink in your path with clang or clang++ and have it use a different binary
+# In that way you can install ccache or buildcache and get much faster compiles...
+sed -i -e $'s/react_native_post_install(installer)/react_native_post_install(installer)\\\n    \\\n    installer.pods_project.targets.each do |target|\\\n      target.build_configurations.each do |config|\\\n        config.build_settings["CC"] = "clang"\\\n        config.build_settings["LD"] = "clang"\\\n        config.build_settings["CXX"] = "clang++"\\\n        config.build_settings["LDPLUSPLUS"] = "clang++"\\\n      end\\\n    end/' ios/Podfile
+rm -f ios/Podfile??
+
+
+#########################################################################
+#
+# react-native-fbsdk-next specific configuration
+#
+
+# Adding the facebook App Id, App Name, are minimum steps
+
+# note the app id used here does not work fully - replace with your own - but it *does* initialize and prove things to a point.
+sed -i -e 's/<\/resources>/    <string name="facebook_app_id">1523458767976650<\/string>\n<\/resources>/' android/app/src/main/res/values/strings.xml
+rm -f android/app/src/main/res/values/strings.xml??
+sed -i -e 's/^<\/dict>/  <key>FacebookDisplayName<\/key>\n  <string>RNFBSDKExample<\/string>\n<\/dict>/' ios/RNFBSDKExample/Info.plist
+rm -f ios/RNFBSDKExample/Info.plist??
+sed -i -e 's/^<\/dict>/  <key>FacebookAppID<\/key>\n  <string>1523458767976650<\/string>\n<\/dict>/' ios/RNFBSDKExample/Info.plist
+rm -f ios/RNFBSDKExample/Info.plist??
+sed -i -e 's/<\/application>/  <meta-data android:name="com.facebook.sdk.ApplicationId" android:value="@string\/facebook_app_id"\/>\n    <\/application>/' android/app/src/main/AndroidManifest.xml
+rm -f android/app/src/main/AndroidManifest.xml??
+sed -i -e 's/<\/application>/  <meta-data android:name="com.facebook.sdk.ApplicationName" android:value="@string\/app_name"\/>\n    <\/application>/' android/app/src/main/AndroidManifest.xml
+rm -f android/app/src/main/AndroidManifest.xml??
+
+# You may optionally want to handle facebook app links
+sed -i -e 's/<\/resources>/    <string name="fb_auto_applink_scheme">fb1523458767976650<\/string>\n<\/resources>/' android/app/src/main/res/values/strings.xml
+rm -f android/app/src/main/res/values/strings.xml??
+sed -i -e 's/<\/intent-filter>/<\/intent-filter>\n        <intent-filter>\n            <action android:name="android.intent.action.VIEW" \/>\n            <category android:name="android.intent.category.DEFAULT" \/>\n            <category android:name="android.intent.category.BROWSABLE" \/>\n            <data android:host="applinks" android:scheme="@string\/fb_auto_applink_scheme" \/>\n        <\/intent-filter>/' android/app/src/main/AndroidManifest.xml
+rm -f android/app/src/main/AndroidManifest.xml??
+
+# Facebook App Client Token will be required with Facebook SDKs 13+
+sed -i -e 's/<\/resources>/    <string name="facebook_client_token">d157902d0bff64b4c1daaf26e32852e9<\/string>\n<\/resources>/' android/app/src/main/res/values/strings.xml
+rm -f android/app/src/main/res/values/strings.xml??
+sed -i -e 's/<\/application>/  <meta-data android:name="com.facebook.sdk.ClientToken" android:value="@string\/facebook_client_token"\/>\n    <\/application>/' android/app/src/main/AndroidManifest.xml
+rm -f android/app/src/main/AndroidManifest.xml??
+sed -i -e 's/^<\/dict>/  <key>FacebookClientToken<\/key>\n  <string>d157902d0bff64b4c1daaf26e32852e9<\/string>\n<\/dict>/' ios/RNFBSDKExample/Info.plist
+rm -f ios/RNFBSDKExample/Info.plist??
+
+# On Android you must add the CustomTabActivity (deprecated WebView)
+sed -i -e 's/<\/activity>/<\/activity>\n      <activity android:name="com.facebook.CustomTabActivity" android:exported="true">\n        <intent-filter>\n          <action android:name="android.intent.action.VIEW" \/>\n          <category android:name="android.intent.category.DEFAULT" \/>\n          <category android:name="android.intent.category.BROWSABLE" \/>\n          <data android:scheme="@string\/fb_login_protocol_scheme" \/>\n        <\/intent-filter>\n      <\/activity>/' android/app/src/main/AndroidManifest.xml
+rm -f android/app/src/main/AndroidManifest.xml??
+
+# This is required for login to work on android and is listed as a requirement for the iOS SDK
+sed -i -e 's/<\/resources>/    <string name="fb_login_protocol_scheme">1523458767976650<\/string>\n<\/resources>/' android/app/src/main/res/values/strings.xml
+rm -f android/app/src/main/res/values/strings.xml??
+
+sed -i -e 's/^<\/dict>/  <key>LSApplicationQueriesSchemes<\/key>\n  <array>\n    <string>fb-messenger-share-api<\/string>\n    <string>fbauth2<\/string>\n    <string>fbapi<\/string>\n  <\/array>\n<\/dict>/' ios/RNFBSDKExample/Info.plist
+rm -f ios/RNFBSDKExample/Info.plist??
+
+# On both platforms you probably need to turn off auto collection and advertiser ID for privacy compliance followed by opt-in / programmatic enable
+sed -i -e 's/<\/application>/  <meta-data android:name="com.facebook.sdk.AutoLogAppEventsEnabled" android:value="false"\/>\n    <\/application>/' android/app/src/main/AndroidManifest.xml
+rm -f android/app/src/main/AndroidManifest.xml??
+sed -i -e 's/<\/application>/  <meta-data android:name="com.facebook.sdk.AdvertiserIDCollectionEnabled" android:value="false"\/>\n    <\/application>/' android/app/src/main/AndroidManifest.xml
+rm -f android/app/src/main/AndroidManifest.xml??
+sed -i -e 's/^<\/dict>/  <key>FacebookAutoLogAppEventsEnabled<\/key>\n  <string>FALSE<\/string>\n<\/dict>/' ios/RNFBSDKExample/Info.plist
+rm -f ios/RNFBSDKExample/Info.plist??
+sed -i -e 's/^<\/dict>/  <key>FacebookAdvertiserIDCollectionEnabled<\/key>\n  <string>FALSE<\/string>\n<\/dict>/' ios/RNFBSDKExample/Info.plist
+rm -f ios/RNFBSDKExample/Info.plist??
+
+# On iOS and Android you have to add entries for various query schemes (Android S+) and allowed URLs etc
+sed -i -e 's/<\/manifest>/    <queries>\n      <provider android:authorities="com.facebook.katana.provider.PlatformProvider" \/>\n      <provider android:authorities="com.facebook.orca.provider.PlatformProvider" \/>\n    <\/queries>\n<\/manifest>/' android/app/src/main/AndroidManifest.xml
+rm -f android/app/src/main/AndroidManifest.xml??
+sed -i -e 's/<string>fbapi<\/string>/<string>fbapi<\/string>\n    <string>fb-messenger-share-api<\/string>\n    <string>fbauth2<\/string>\n    <string>fbapi<\/string>\n    <string>fbapi20130214<\/string>\n    <string>fbapi20130410<\/string>\n    <string>fbapi20130702<\/string>\n    <string>fbapi20131010<\/string>\n    <string>fbapi20131219<\/string>\n    <string>fbapi20140410<\/string>\n    <string>fbapi20140116<\/string>\n    <string>fbapi20150313<\/string>\n    <string>fbapi20150629<\/string>\n    <string>fbapi20160328<\/string>\n    <string>fbauth<\/string>\n    <string>fbshareextension<\/string>/' ios/RNFBSDKExample/Info.plist
+rm -f ios/RNFBSDKExample/Info.plist??
+sed -i -e 's/^<\/dict>/  <key>CFBundleURLTypes<\/key>\n  <array>\n    <dict>\n     <key>CFBundleURLSchemes<\/key>\n     <array>\n       <string>fb1523458767976650<\/string>\n     <\/array>\n   <\/dict>\n  <\/array>\n<\/dict>/' ios/RNFBSDKExample/Info.plist
+rm -f ios/RNFBSDKExample/Info.plist??
+
+# You may want to integrate Facebook's content provider
+sed -i -e 's/<\/application>/  <provider android:name="com.facebook.FacebookContentProvider" android:authorities="com.facebook.app.FacebookContentProvider355198514515820" android:exported="true" \/>\n    <\/application>/' android/app/src/main/AndroidManifest.xml
+rm -f android/app/src/main/AndroidManifest.xml??
+
+
+# Install the results
 pushd ios && pod deintegrate && pod install && popd
 
 # Copy the important files back in
