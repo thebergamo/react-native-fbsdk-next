4b0875e61516ebd56f6df089764ea6ede7dc3078
diego <diegooliveira.tsi@gmail.com>
2021-11-19 10:38:47 -0300

    feat: create mocks to appevents and params

---
+++ jestSetup.js
@@ -0,0 +1,23 @@
+
+
+import {mockAppEventParams, mockAppEvents} from './mocks';
+
+jest.mock('./src', () => {
+  const nonMocked = jest.requireActual('./src');
+  return {
+    AppEventsLogger: {
+      logEvent: jest.fn(),
+      logPurchase: jest.fn(),
+      setUserID: jest.fn(),
+      AppEventParams: mockAppEventParams,
+      AppEvents: mockAppEvents,
+    },
+    LoginManager: {
+      logInWithPermissions: jest.fn(),
+    },
+    Settings: {
+      initializeSDK: jest.fn(),
+    },
+    LoginButton: nonMocked.LoginButton,
+  };
+});
