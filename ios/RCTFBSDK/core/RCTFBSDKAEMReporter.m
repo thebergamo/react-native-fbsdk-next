0d76349b3aa64b1a5baccf533ab6e134648faae5
Mike <yum650350@gmail.com>
2021-12-23 00:18:58 +0800

    feat: AEM logger
    * fix project.pbxproj
    
    * implement logAEMEvent for iOS
    
    * documentation for AEM

---
+++ ios/RCTFBSDK/core/RCTFBSDKAEMReporter.m
@@ -0,0 +1,30 @@
+//
+//  RCTFBSDKAEMReporter.m
+//  react-native-fbsdk-next
+//
+//  Created by rnike@gitub on 2021/12/17.
+//
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTUtils.h>
+#import <FBAEMKit/FBAEMReporter.h>
+
+@interface RCTFBSDKAEMReporter : NSObject <RCTBridgeModule>
+@end
+
+@implementation RCTFBSDKAEMReporter
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(recordAndUpdateEvent:(NSString *)eventName
+                  value:(NSNumber * __nonnull)value
+                  currency:(NSString * _Nullable)currency
+                  otherParameters:(NSDictionary<NSString *,id> * _Nullable)otherParameters)
+{
+    [FBAEMReporter recordAndUpdateEvent:eventName
+                               currency:currency
+                                  value:value
+                             parameters:otherParameters];
+}
+
+@end
