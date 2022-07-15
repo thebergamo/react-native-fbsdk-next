e758039f5acac91c869efcd479f82af94cdc6072
hiepnsx <hiep@verve-inc.jp>
2022-07-15 18:50:24 +0900

    fix: Profile.getCurrentProfile() always returns null in the first time

--- android/src/main/java/com/facebook/reactnative/androidsdk/FBProfileModule.java
+++ android/src/main/java/com/facebook/reactnative/androidsdk/FBProfileModule.java
@@ -1,6 +1,7 @@
 package com.facebook.reactnative.androidsdk;
 
 import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -10,12 +11,16 @@ import com.facebook.react.module.annotations.ReactModule;
 
 import androidx.annotation.NonNull;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * This is a {@link NativeModule} that allows JS to use FBSDKProfile info of the current logged user.
  */
 @ReactModule(name = FBProfileModule.NAME)
 public class FBProfileModule extends ReactContextBaseJavaModule {
     public static final String NAME = "FBProfile";
+    private ProfileTracker mProfileTracker;
 
     public FBProfileModule(ReactApplicationContext reactContext) {
       super(reactContext);
@@ -28,14 +33,36 @@ public class FBProfileModule extends ReactContextBaseJavaModule {
   }
 
     /**
-    * Get the current logged profile.
-    * @param callback Use callback to pass the current logged profile back to JS.
-    */
+     * Get the current logged profile.
+     * @param callback Use callback to pass the current logged profile back to JS.
+     */
     @ReactMethod
-    public void getCurrentProfile(Callback callback) {
-        //Return the profile object as a ReactMap.
-        callback.invoke(Profile.getCurrentProfile() == null
-                ? null
-                : Utility.profileToReactMap(Profile.getCurrentProfile()));
+    public void getCurrentProfile(final Callback callback) {
+      // Return the profile object as a ReactMap.
+      if (Profile.getCurrentProfile() == null) {
+        final Timer timer = new Timer();
+        synchronized (timer) {
+          timer.schedule(
+            new TimerTask() {
+              @Override
+              public void run() {
+                timer.cancel();
+                callback.invoke(null);
+              }
+            },
+            30000
+          );
+          mProfileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+              timer.cancel();
+              mProfileTracker.stopTracking();
+              callback.invoke(Utility.profileToReactMap(currentProfile));
+            }
+          };
+        }
+      } else {
+        callback.invoke(Utility.profileToReactMap(Profile.getCurrentProfile()));
+      }
   }
 }
