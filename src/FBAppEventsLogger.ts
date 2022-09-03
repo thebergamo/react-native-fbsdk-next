3e8977a35f2215ca1a6369e7faec9da0ef621fd3
Danilo Bürger <info@danilobuerger.de>
2022-09-03 15:27:10 +0200

    fix(android): remove setUserID(null) android workaround (#307)
    Upstream fix was released in android sdk 13.1.0

--- src/FBAppEventsLogger.ts
+++ src/FBAppEventsLogger.ts
@@ -293,11 +293,6 @@ export default {
    * The userID is persisted until this method is called again with a null userId
    */
   setUserID(userID: string | null) {
-    if (userID === null && Platform.OS === 'android') {
-      // TODO: Remove workaround once https://github.com/facebook/facebook-android-sdk/issues/1047 is released
-      AppEventsLogger.clearUserID();
-      return;
-    }
     AppEventsLogger.setUserID(userID);
   },
 
