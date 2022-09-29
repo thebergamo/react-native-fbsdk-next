aee5565329b07c93a02bace9b646a4096aaa8903
Mike Hardy <github@mikehardy.net>
2022-09-29 10:41:10 -0500

    chore(example): run refresh-example, forward port Podfile tweaks to RN0.70
    also, "--no-jetifier" is no longer a valid option, RIP jetifier, long live jetifier

--- refresh-example.sh
+++ refresh-example.sh
@@ -35,27 +35,22 @@ yarn add github:thebergamo/react-native-fbsdk-next
 #
 
 # We want to test Android12 compatibility, so let's bump up our compile and target versions on android:
-sed -i -e $'s/compileSdkVersion = 30/compileSdkVersion = 31/' android/build.gradle
-sed -i -e $'s/targetSdkVersion = 30/targetSdkVersion = 31/' android/build.gradle
+sed -i -e $'s/compileSdkVersion = 30/compileSdkVersion = 33/' android/build.gradle
+sed -i -e $'s/targetSdkVersion = 30/targetSdkVersion = 33/' android/build.gradle
 rm -f android/build.gradle??
-# Android 12 does require a tweak to the stock template AndroidManifests, add exported to all activities for compliance
-sed -i -e $'s/android:launchMode/android:exported="true"\\\n        android:launchMode/' android/app/src/main/AndroidManifest.xml
-rm -f android/app/src/main/AndroidManifest.xml??
-sed -i -e $'s/DevSettingsActivity"/DevSettingsActivity" android:exported="true"/' android/app/src/debug/AndroidManifest.xml
-rm -f android/app/src/debug/AndroidManifest.xml??
 
 # React-native builds on iOS are very noisy with warnings in other packages that drown our warnings out. Reduce warnings to just our packages.
-sed -i -e $'s/react_native_post_install(installer)/react_native_post_install(installer)\\\n\\\n    # quiet non-module warnings - only interested in fbsdk-next warnings\\\n    installer.pods_project.targets.each do |target|\\\n      if !target.name.include? "react-native-fbsdk-next"\\\n        target.build_configurations.each do |config|\\\n          config.build_settings["GCC_WARN_INHIBIT_ALL_WARNINGS"] = "YES"\\\n        end\\\n      end\\\n    end/' ios/Podfile
+sed -i -e $'s/__apply_Xcode_12_5_M1_post_install_workaround(installer)/__apply_Xcode_12_5_M1_post_install_workaround(installer)\\\n\\\n    # quiet non-module warnings - only interested in fbsdk-next warnings\\\n    installer.pods_project.targets.each do |target|\\\n      if !target.name.include? "react-native-fbsdk-next"\\\n        target.build_configurations.each do |config|\\\n          config.build_settings["GCC_WARN_INHIBIT_ALL_WARNINGS"] = "YES"\\\n        end\\\n      end\\\n    end/' ios/Podfile
 rm -f ios/Podfile??
 
-# Apple builds in general have a problem with architectures on Apple Silicon and Intel, and doing some exclusions should help
-sed -i -e $'s/react_native_post_install(installer)/react_native_post_install(installer)\\\n\\\n    # work around known build issues on M1 macs\\\n    installer.aggregate_targets.each do |aggregate_target|\\\n      aggregate_target.user_project.native_targets.each do |target|\\\n        target.build_configurations.each do |config|\\\n          config.build_settings[\'ONLY_ACTIVE_ARCH\'] = \'YES\'\\\n          config.build_settings[\'EXCLUDED_ARCHS\'] = \'i386\'\\\n        end\\\n      end\\\n      aggregate_target.user_project.save\\\n    end/' ios/Podfile
-rm -f ios/Podfile??
+# Optional: Apple M1 workaround - builds may have a problem with architectures on Apple Silicon and Intel, some exclusions may help
+sed -i -e $'s/__apply_Xcode_12_5_M1_post_install_workaround(installer)/__apply_Xcode_12_5_M1_post_install_workaround(installer)\\\n    \\\n    installer.aggregate_targets.each do |aggregate_target|\\\n      aggregate_target.user_project.native_targets.each do |target|\\\n        target.build_configurations.each do |config|\\\n          config.build_settings[\'ONLY_ACTIVE_ARCH\'] = \'YES\'\\\n          config.build_settings[\'EXCLUDED_ARCHS\'] = \'i386\'\\\n        end\\\n      end\\\n      aggregate_target.user_project.save\\\n    end/' ios/Podfile
+rm -f ios/Podfile.??
 
-# This is just a speed optimization, very optional, but asks xcodebuild to use clang and clang++ without the fully-qualified path
+# Optional: build performance optimization to use ccache - asks xcodebuild to use clang and clang++ without the fully-qualified path
 # That means that you can then make a symlink in your path with clang or clang++ and have it use a different binary
 # In that way you can install ccache or buildcache and get much faster compiles...
-sed -i -e $'s/react_native_post_install(installer)/react_native_post_install(installer)\\\n\\\n    # allow compiler wrappers in PATH to work (for example, ccache)\\\n    installer.pods_project.targets.each do |target|\\\n      target.build_configurations.each do |config|\\\n        config.build_settings["CC"] = "clang"\\\n        config.build_settings["LD"] = "clang"\\\n        config.build_settings["CXX"] = "clang++"\\\n        config.build_settings["LDPLUSPLUS"] = "clang++"\\\n      end\\\n    end/' ios/Podfile
+sed -i -e $'s/__apply_Xcode_12_5_M1_post_install_workaround(installer)/__apply_Xcode_12_5_M1_post_install_workaround(installer)\\\n    \\\n    installer.pods_project.targets.each do |target|\\\n      target.build_configurations.each do |config|\\\n        config.build_settings["CC"] = "clang"\\\n        config.build_settings["LD"] = "clang"\\\n        config.build_settings["CXX"] = "clang++"\\\n        config.build_settings["LDPLUSPLUS"] = "clang++"\\\n      end\\\n    end/' ios/Podfile
 rm -f ios/Podfile??
 
 
