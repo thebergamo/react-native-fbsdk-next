0ca1229e9244cbfb24eb527b9bde349d63e5b5c6
Mike Hardy <github@mikehardy.net>
2024-09-26 17:41:33 -0500

    build(deps): update most javascript dependencies
    of note:
    - typescript updated, and that updated the plugin's built artifact
    - semantic release stuff all moved across majors, deserves a close watch during release

--- plugin/build/config.js
+++ plugin/build/config.js
@@ -1,6 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getFacebookAdvertiserIDCollection = exports.getFacebookAutoLogAppEvents = exports.getFacebookAutoInitEnabled = exports.getFacebookDisplayName = exports.getFacebookScheme = exports.getFacebookClientToken = exports.getFacebookAppId = exports.getMergePropsWithConfig = void 0;
+exports.getMergePropsWithConfig = getMergePropsWithConfig;
+exports.getFacebookAppId = getFacebookAppId;
+exports.getFacebookClientToken = getFacebookClientToken;
+exports.getFacebookScheme = getFacebookScheme;
+exports.getFacebookDisplayName = getFacebookDisplayName;
+exports.getFacebookAutoInitEnabled = getFacebookAutoInitEnabled;
+exports.getFacebookAutoLogAppEvents = getFacebookAutoLogAppEvents;
+exports.getFacebookAdvertiserIDCollection = getFacebookAdvertiserIDCollection;
 function getMergePropsWithConfig(config, props) {
     const { facebookAppId, facebookDisplayName, facebookScheme, facebookAutoInitEnabled, facebookAutoLogAppEventsEnabled, facebookAdvertiserIDCollectionEnabled, } = config;
     const { appID = facebookAppId, clientToken, displayName = facebookDisplayName, scheme = facebookScheme ?? (appID ? `fb${appID}` : undefined), isAutoInitEnabled = facebookAutoInitEnabled ?? false, autoLogAppEventsEnabled = facebookAutoLogAppEventsEnabled ?? false, advertiserIDCollectionEnabled = facebookAdvertiserIDCollectionEnabled ??
@@ -16,32 +23,24 @@ function getMergePropsWithConfig(config, props) {
         iosUserTrackingPermission,
     };
 }
-exports.getMergePropsWithConfig = getMergePropsWithConfig;
 function getFacebookAppId(config) {
     return config.appID ?? null;
 }
-exports.getFacebookAppId = getFacebookAppId;
 function getFacebookClientToken(config) {
     return config.clientToken ?? null;
 }
-exports.getFacebookClientToken = getFacebookClientToken;
 function getFacebookScheme(config) {
     return config.scheme ?? null;
 }
-exports.getFacebookScheme = getFacebookScheme;
 function getFacebookDisplayName(config) {
     return config.displayName ?? null;
 }
-exports.getFacebookDisplayName = getFacebookDisplayName;
 function getFacebookAutoInitEnabled(config) {
     return config.isAutoInitEnabled ?? null;
 }
-exports.getFacebookAutoInitEnabled = getFacebookAutoInitEnabled;
 function getFacebookAutoLogAppEvents(config) {
     return config.autoLogAppEventsEnabled ?? null;
 }
-exports.getFacebookAutoLogAppEvents = getFacebookAutoLogAppEvents;
 function getFacebookAdvertiserIDCollection(config) {
     return config.advertiserIDCollectionEnabled ?? null;
 }
-exports.getFacebookAdvertiserIDCollection = getFacebookAdvertiserIDCollection;
--- plugin/build/withFacebookIOS.js
+++ plugin/build/withFacebookIOS.js
@@ -1,6 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.withUserTrackingPermission = exports.setFacebookApplicationQuerySchemes = exports.setFacebookDisplayName = exports.setFacebookClientToken = exports.setFacebookAppId = exports.setFacebookAdvertiserIDCollectionEnabled = exports.setFacebookAutoLogAppEventsEnabled = exports.setFacebookAutoInitEnabled = exports.setFacebookScheme = exports.setFacebookConfig = exports.withFacebookIOS = void 0;
+exports.withUserTrackingPermission = exports.withFacebookIOS = void 0;
+exports.setFacebookConfig = setFacebookConfig;
+exports.setFacebookScheme = setFacebookScheme;
+exports.setFacebookAutoInitEnabled = setFacebookAutoInitEnabled;
+exports.setFacebookAutoLogAppEventsEnabled = setFacebookAutoLogAppEventsEnabled;
+exports.setFacebookAdvertiserIDCollectionEnabled = setFacebookAdvertiserIDCollectionEnabled;
+exports.setFacebookAppId = setFacebookAppId;
+exports.setFacebookClientToken = setFacebookClientToken;
+exports.setFacebookDisplayName = setFacebookDisplayName;
+exports.setFacebookApplicationQuerySchemes = setFacebookApplicationQuerySchemes;
 const config_1 = require("./config");
 const config_plugins_1 = require("@expo/config-plugins");
 const { Scheme } = config_plugins_1.IOSConfig;
@@ -24,7 +33,6 @@ function setFacebookConfig(config, infoPlist) {
     infoPlist = setFacebookScheme(config, infoPlist);
     return infoPlist;
 }
-exports.setFacebookConfig = setFacebookConfig;
 function setFacebookScheme(config, infoPlist) {
     const facebookScheme = (0, config_1.getFacebookScheme)(config);
     if (!facebookScheme) {
@@ -35,7 +43,6 @@ function setFacebookScheme(config, infoPlist) {
     }
     return appendScheme(facebookScheme, infoPlist);
 }
-exports.setFacebookScheme = setFacebookScheme;
 function setFacebookAutoInitEnabled(config, { FacebookAutoInitEnabled: _, ...infoPlist }) {
     const isAutoInitEnabled = (0, config_1.getFacebookAutoInitEnabled)(config);
     if (isAutoInitEnabled === null) {
@@ -46,7 +53,6 @@ function setFacebookAutoInitEnabled(config, { FacebookAutoInitEnabled: _, ...inf
         FacebookAutoInitEnabled: isAutoInitEnabled,
     };
 }
-exports.setFacebookAutoInitEnabled = setFacebookAutoInitEnabled;
 function setFacebookAutoLogAppEventsEnabled(config, { FacebookAutoLogAppEventsEnabled: _, ...infoPlist }) {
     const autoLogAppEventsEnabled = (0, config_1.getFacebookAutoLogAppEvents)(config);
     if (autoLogAppEventsEnabled === null) {
@@ -57,7 +63,6 @@ function setFacebookAutoLogAppEventsEnabled(config, { FacebookAutoLogAppEventsEn
         FacebookAutoLogAppEventsEnabled: autoLogAppEventsEnabled,
     };
 }
-exports.setFacebookAutoLogAppEventsEnabled = setFacebookAutoLogAppEventsEnabled;
 function setFacebookAdvertiserIDCollectionEnabled(config, { FacebookAdvertiserIDCollectionEnabled: _, ...infoPlist }) {
     const advertiserIDCollectionEnabled = (0, config_1.getFacebookAdvertiserIDCollection)(config);
     if (advertiserIDCollectionEnabled === null) {
@@ -68,7 +73,6 @@ function setFacebookAdvertiserIDCollectionEnabled(config, { FacebookAdvertiserID
         FacebookAdvertiserIDCollectionEnabled: advertiserIDCollectionEnabled,
     };
 }
-exports.setFacebookAdvertiserIDCollectionEnabled = setFacebookAdvertiserIDCollectionEnabled;
 function setFacebookAppId(config, { FacebookAppID: _, ...infoPlist }) {
     const appID = (0, config_1.getFacebookAppId)(config);
     if (appID) {
@@ -79,7 +83,6 @@ function setFacebookAppId(config, { FacebookAppID: _, ...infoPlist }) {
     }
     return infoPlist;
 }
-exports.setFacebookAppId = setFacebookAppId;
 function setFacebookClientToken(config, { FacebookClientToken: _, ...infoPlist }) {
     const clientToken = (0, config_1.getFacebookClientToken)(config);
     if (clientToken) {
@@ -90,7 +93,6 @@ function setFacebookClientToken(config, { FacebookClientToken: _, ...infoPlist }
     }
     return infoPlist;
 }
-exports.setFacebookClientToken = setFacebookClientToken;
 function setFacebookDisplayName(config, { FacebookDisplayName: _, ...infoPlist }) {
     const facebookDisplayName = (0, config_1.getFacebookDisplayName)(config);
     if (facebookDisplayName) {
@@ -101,7 +103,6 @@ function setFacebookDisplayName(config, { FacebookDisplayName: _, ...infoPlist }
     }
     return infoPlist;
 }
-exports.setFacebookDisplayName = setFacebookDisplayName;
 function setFacebookApplicationQuerySchemes(config, infoPlist) {
     const facebookAppId = (0, config_1.getFacebookAppId)(config);
     const existingSchemes = infoPlist.LSApplicationQueriesSchemes || [];
@@ -144,7 +145,6 @@ function setFacebookApplicationQuerySchemes(config, infoPlist) {
         LSApplicationQueriesSchemes: updatedSchemes,
     };
 }
-exports.setFacebookApplicationQuerySchemes = setFacebookApplicationQuerySchemes;
 const withUserTrackingPermission = (config, { iosUserTrackingPermission } = {}) => {
     if (!iosUserTrackingPermission) {
         return config;
