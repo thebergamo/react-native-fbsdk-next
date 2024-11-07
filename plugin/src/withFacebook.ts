cf337f18386652fd273834f2d9f6aedda88e1706
LukeGu <luke.gu.work@gmail.com>
2024-11-07 14:52:55 +1100

    fix(expo): update facebook plugin config

--- plugin/src/withFacebook.ts
+++ plugin/src/withFacebook.ts
@@ -6,13 +6,43 @@ import {
 } from './withFacebookAndroid';
 import {withFacebookIOS, withUserTrackingPermission} from './withFacebookIOS';
 import {withSKAdNetworkIdentifiers} from './withSKAdNetworkIdentifiers';
+import {ExpoConfig} from '@expo/config-types';
 import {ConfigPlugin, createRunOncePlugin} from '@expo/config-plugins';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const pkg = require('react-native-fbsdk-next/package.json');
 
+function getExpoFacebookConfig(config: ExpoConfig) {
+  const facebookPlugin = config.plugins?.find((plugin) => {
+    if (Array.isArray(plugin) && plugin[0] === 'facebook') {
+      return true;
+    }
+    return false;
+  });
+
+  if (Array.isArray(facebookPlugin) && facebookPlugin.length > 1) {
+    return {
+      plugins: {
+        facebook: facebookPlugin[1],
+      },
+    };
+  }
+
+  return {
+    plugins: {
+      facebook: {},
+    },
+  };
+}
+
 const withFacebook: ConfigPlugin<ConfigProps | void> = (config, props) => {
-  const newProps = getMergePropsWithConfig(config, props);
+  // Convert ExpoConfig to ExpoConfigFacebook format
+  const facebookConfig = getExpoFacebookConfig(config);
+
+  // Merge the configs
+  const newProps = getMergePropsWithConfig(facebookConfig, props);
+
+  // Validation
   if (!newProps.appID) {
     throw new Error('missing appID in the plugin properties');
   }
