thebergamo/react-native-fbsdk-next

commit 7fda601290065e8a5f203094907cb13341bff8cd

feat: add support for new architecture in compatibility mode (#505)

ios/RCTFBSDK/login/RCTFBSDKLoginButtonManager.m
@@ -17,8 +17,8 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import "RCTFBSDKLoginButtonManager.h"
+#import "RCTFBSDKLoginButtonView.h"
 
-#import <React/RCTBridge.h>
 #import <React/RCTComponentEvent.h>
 #import <React/RCTEventDispatcher.h>
 #import <React/RCTUtils.h>
@@ -34,12 +34,12 @@ RCT_EXPORT_MODULE(RCTFBLoginButton)
 
 - (UIView *)view
 {
-  FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
-  loginButton.delegate = self;
-  return loginButton;
+  return [[RCTFBSDKLoginButtonView alloc] init];
 }
 
 #pragma mark - Properties
+
+RCT_EXPORT_VIEW_PROPERTY(onChange, RCTBubblingEventBlock)
 
 RCT_EXPORT_VIEW_PROPERTY(permissions, NSStringArray)
 
@@ -60,36 +60,5 @@ RCT_CUSTOM_VIEW_PROPERTY(tooltipBehaviorIOS, FBSDKLoginButtonTooltipBehavior, FB
   [view setTooltipBehavior:json ? [RCTConvert FBSDKLoginButtonTooltipBehavior:json] : FBSDKLoginButtonTooltipBehaviorAutomatic];
 }
 
-#pragma mark - FBSDKLoginButtonDelegate
-
-- (void)loginButton:(FBSDKLoginButton *)loginButton didCompleteWithResult:(FBSDKLoginManagerLoginResult *)result error:(NSError *)error
-{
-  NSDictionary *body = @{
-    @"type": @"loginFinished",
-    @"error": error ? RCTJSErrorFromNSError(error) : [NSNull null],
-    @"result": error ? [NSNull null] : @{
-      @"isCancelled": @(result.isCancelled),
-      @"grantedPermissions": result.isCancelled ? [NSNull null] : result.grantedPermissions.allObjects,
-      @"declinedPermissions": result.isCancelled ? [NSNull null] : result.declinedPermissions.allObjects,
-    },
-  };
-
-  RCTComponentEvent *event = [[RCTComponentEvent alloc] initWithName:@"topChange"
-                                                             viewTag:loginButton.reactTag
-                                                                body:body];
-  [self.bridge.eventDispatcher sendEvent:event];
-}
-
-- (void)loginButtonDidLogOut:(FBSDKLoginButton *)loginButton
-{
-  NSDictionary *body = @{
-    @"type": @"logoutFinished",
-  };
-
-  RCTComponentEvent *event = [[RCTComponentEvent alloc] initWithName:@"topChange"
-                                                             viewTag:loginButton.reactTag
-                                                                body:body];
-  [self.bridge.eventDispatcher sendEvent:event];
-}
 
 @end

ios/RCTFBSDK/login/RCTFBSDKLoginButtonView.h
@@ -1,0 +1,10 @@
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#import <React/RCTComponent.h>
+#import <React/RCTEventDispatcher.h>
+
+@interface RCTFBSDKLoginButtonView: UIView<FBSDKLoginButtonDelegate>
+
+@property (nonatomic, copy) RCTBubblingEventBlock onChange;
+
+@end

ios/RCTFBSDK/login/RCTFBSDKLoginButtonView.m
@@ -1,0 +1,59 @@
+#import "RCTFBSDKLoginButtonView.h"
+
+#import <React/RCTComponentEvent.h>
+#import <React/RCTEventDispatcher.h>
+#import <React/RCTUtils.h>
+#import <React/UIView+React.h>
+
+
+@interface RCTFBSDKLoginButtonView ()
+
+@property (nonatomic, strong) FBSDKLoginButton *loginButton;
+
+@end
+
+@implementation RCTFBSDKLoginButtonView
+
+- (instancetype)init
+{
+  self = [super init];
+  if (self) {
+    self.loginButton = [[FBSDKLoginButton alloc] init];
+    self.loginButton.delegate = self;
+    self.loginButton.frame = self.bounds;
+    self.loginButton.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+
+    [self addSubview:_loginButton];
+
+    return self;
+  }
+  return self;
+}
+
+#pragma mark - FBSDKLoginButtonDelegate
+
+- (void)loginButton:(FBSDKLoginButton *)loginButton didCompleteWithResult:(FBSDKLoginManagerLoginResult *)result error:(NSError *)error
+{
+  NSDictionary *body = @{
+    @"type": @"loginFinished",
+    @"error": error ? RCTJSErrorFromNSError(error) : [NSNull null],
+    @"result": error ? [NSNull null] : @{
+      @"isCancelled": @(result.isCancelled),
+      @"grantedPermissions": result.isCancelled ? [NSNull null] : result.grantedPermissions.allObjects,
+      @"declinedPermissions": result.isCancelled ? [NSNull null] : result.declinedPermissions.allObjects,
+    },
+  };
+
+  self.onChange(body);
+}
+
+- (void)loginButtonDidLogOut:(FBSDKLoginButton *)loginButton
+{
+  NSDictionary *body = @{
+    @"type": @"logoutFinished",
+  };
+
+  self.onChange(body);
+}
+
+@end

android/src/main/java/com/facebook/reactnative/androidsdk/FBSettingsModule.java
@@ -51,7 +51,7 @@ public class FBSettingsModule extends BaseJavaModule {
      * [FB SDK Best Practices for GDPR Compliance](https://developers.facebook.com/docs/app-events/gdpr-compliance/)
      */
     @ReactMethod
-    public static void initializeSDK() {
+    public void initializeSDK() {
         FacebookSdk.fullyInitialize();
     }
 
@@ -60,7 +60,7 @@ public class FBSettingsModule extends BaseJavaModule {
      * @param appID app id
      */
     @ReactMethod
-    public static void setAppID(String appID) {
+    public void setAppID(String appID) {
         FacebookSdk.setApplicationId(appID);
     }
 
@@ -69,27 +69,27 @@ public class FBSettingsModule extends BaseJavaModule {
      * @param clientToken client token
      */
     @ReactMethod
-    public static void setClientToken(String clientToken) {
+    public void setClientToken(String clientToken) {
         FacebookSdk.setClientToken(clientToken);
     }
 
     @ReactMethod
-    public static void setAppName(String displayName) {
+    public void setAppName(String displayName) {
         FacebookSdk.setApplicationName(displayName);
     }
 
     @ReactMethod
-    public static void setGraphAPIVersion(String version) {
+    public void setGraphAPIVersion(String version) {
         FacebookSdk.setGraphApiVersion(version);
     }
 
     @ReactMethod
-    public static void setAutoLogAppEventsEnabled(Boolean enabled) {
+    public void setAutoLogAppEventsEnabled(Boolean enabled) {
         FacebookSdk.setAutoLogAppEventsEnabled(enabled);
     }
 
     @ReactMethod
-    public static void setAdvertiserIDCollectionEnabled(Boolean enabled) {
+    public void setAdvertiserIDCollectionEnabled(Boolean enabled) {
         FacebookSdk.setAdvertiserIDCollectionEnabled(enabled);
     }
 }

android/src/main/java/com/facebook/reactnative/androidsdk/RCTLoginButton.java
@@ -31,7 +31,8 @@ import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.uimanager.ThemedReactContext;
-import com.facebook.react.uimanager.events.RCTEventEmitter;
+import com.facebook.react.uimanager.UIManagerHelper;
+import com.facebook.react.uimanager.events.EventDispatcher;
 
 import java.util.Set;
 
@@ -42,11 +43,13 @@ import java.util.Set;
 public class RCTLoginButton extends LoginButton {
 
     private final CallbackManager mCallbackManager;
+    private final EventDispatcher mEventDispatcher;
 
     public RCTLoginButton(ThemedReactContext context, CallbackManager callbackManager) {
         super(context);
         this.setToolTipMode(ToolTipMode.NEVER_DISPLAY);
         mCallbackManager = callbackManager;
+        mEventDispatcher = UIManagerHelper.getEventDispatcherForReactTag((ReactContext) getContext(), getId());
         init();
     }
 
@@ -60,10 +63,8 @@ public class RCTLoginButton extends LoginButton {
                     WritableMap event = Arguments.createMap();
                     event.putString("type", "logoutFinished");
                     ReactContext context = (ReactContext) getContext();
-                    context.getJSModule(RCTEventEmitter.class).receiveEvent(
-                            getId(),
-                            "topChange",
-                            event);
+                    mEventDispatcher.dispatchEvent(new RCTLoginButtonEvent(UIManagerHelper.getSurfaceId(context), getId(), event));
+
                 }
             }
         };
@@ -85,10 +86,7 @@ public class RCTLoginButton extends LoginButton {
                                 setToStringArray(loginResult.getRecentlyDeniedPermissions())));
                 event.putMap("result", result);
                 ReactContext context = (ReactContext) getContext();
-                context.getJSModule(RCTEventEmitter.class).receiveEvent(
-                        getId(),
-                        "topChange",
-                        event);
+                mEventDispatcher.dispatchEvent(new RCTLoginButtonEvent(UIManagerHelper.getSurfaceId(context), getId(), event));
             }
 
             @Override
@@ -100,10 +98,7 @@ public class RCTLoginButton extends LoginButton {
                 result.putBoolean("isCancelled", false);
                 event.putMap("result", result);
                 ReactContext context = (ReactContext) getContext();
-                context.getJSModule(RCTEventEmitter.class).receiveEvent(
-                        getId(),
-                        "topChange",
-                        event);
+                mEventDispatcher.dispatchEvent(new RCTLoginButtonEvent(UIManagerHelper.getSurfaceId(context), getId(), event));
             }
 
             @Override
@@ -115,10 +110,7 @@ public class RCTLoginButton extends LoginButton {
                 result.putBoolean("isCancelled", true);
                 event.putMap("result", result);
                 ReactContext context = (ReactContext) getContext();
-                context.getJSModule(RCTEventEmitter.class).receiveEvent(
-                        getId(),
-                        "topChange",
-                        event);
+                mEventDispatcher.dispatchEvent(new RCTLoginButtonEvent(UIManagerHelper.getSurfaceId(context), getId(), event));
             }
         });
     }

android/src/main/java/com/facebook/reactnative/androidsdk/RCTLoginButtonEvent.java
@@ -1,0 +1,27 @@
+package com.facebook.reactnative.androidsdk;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.uimanager.events.Event;
+
+public class RCTLoginButtonEvent extends Event<RCTLoginButtonEvent> {
+    public static final String EVENT_NAME = "topChange";
+    private final WritableMap mEvent;
+
+    public RCTLoginButtonEvent(int surfaceId, int viewTag, WritableMap event) {
+        super(surfaceId,viewTag);
+        mEvent = event;
+    }
+
+    @NonNull
+    @Override
+    public String getEventName() {
+        return EVENT_NAME;
+    }
+
+    @Override
+    protected WritableMap getEventData() {
+        return mEvent;
+    }
+}

ios/RCTFBSDK/login/RCTFBSDKLoginButtonManager.h
@@ -20,6 +20,6 @@
 
 #import <React/RCTViewManager.h>
 
-@interface RCTFBSDKLoginButtonManager : RCTViewManager <FBSDKLoginButtonDelegate>
+@interface RCTFBSDKLoginButtonManager : RCTViewManager
 
 @end

commit 23364baf81cbfdd60ca5af3b2f5f72814e893e59

fix(ios): unrecognized selector sent to instance when using props
A critical issue was introduced by #505 that breaks usage of any of the following props on iOS platform:
- `permissions`
- `defaultAudience`
- `nonceIOS`
- `loginTrackingIOS`
- `tooltipBehaviorIOS`

This commit exposes the underlying `FBSDKLoginButton` property on the custom `RCTFBSDKLoginButtonView` wrapper,
and re-exports these props as custom properties with the correct viewClass for correct static type checking.

ios/RCTFBSDK/login/RCTFBSDKLoginButtonManager.m
@@ -41,23 +41,32 @@ RCT_EXPORT_MODULE(RCTFBLoginButton)
 
 RCT_EXPORT_VIEW_PROPERTY(onChange, RCTBubblingEventBlock)
 
-RCT_EXPORT_VIEW_PROPERTY(permissions, NSStringArray)
-
-RCT_EXPORT_VIEW_PROPERTY(defaultAudience, FBSDKDefaultAudience)
-
-RCT_CUSTOM_VIEW_PROPERTY(nonceIOS, NSString, FBSDKLoginButton)
+RCT_CUSTOM_VIEW_PROPERTY(permissions, NSStringArray, RCTFBSDKLoginButtonView)
 {
-    [view setNonce:json ? json : nil];
+    [view.loginButton setPermissions:json ? json : nil];
 }
 
-RCT_CUSTOM_VIEW_PROPERTY(loginTrackingIOS, NSString, FBSDKLoginButton)
+RCT_CUSTOM_VIEW_PROPERTY(defaultAudience, FBSDKDefaultAudience, RCTFBSDKLoginButtonView)
 {
-    [view setLoginTracking:([json isEqualToString:@"limited"]) ? FBSDKLoginTrackingLimited : FBSDKLoginTrackingEnabled];
+    if (json)
+    {
+        [view.loginButton setDefaultAudience:[RCTConvert FBSDKDefaultAudience:json]];
+    }
 }
 
-RCT_CUSTOM_VIEW_PROPERTY(tooltipBehaviorIOS, FBSDKLoginButtonTooltipBehavior, FBSDKLoginButton)
+RCT_CUSTOM_VIEW_PROPERTY(nonceIOS, NSString, RCTFBSDKLoginButtonView)
 {
-  [view setTooltipBehavior:json ? [RCTConvert FBSDKLoginButtonTooltipBehavior:json] : FBSDKLoginButtonTooltipBehaviorAutomatic];
+    [view.loginButton setNonce:json ? json : nil];
+}
+
+RCT_CUSTOM_VIEW_PROPERTY(loginTrackingIOS, NSString, RCTFBSDKLoginButtonView)
+{
+    [view.loginButton setLoginTracking:([json isEqualToString:@"limited"]) ? FBSDKLoginTrackingLimited : FBSDKLoginTrackingEnabled];
+}
+
+RCT_CUSTOM_VIEW_PROPERTY(tooltipBehaviorIOS, FBSDKLoginButtonTooltipBehavior, RCTFBSDKLoginButtonView)
+{
+  [view.loginButton setTooltipBehavior:json ? [RCTConvert FBSDKLoginButtonTooltipBehavior:json] : FBSDKLoginButtonTooltipBehaviorAutomatic];
 }
 
 

ios/RCTFBSDK/login/RCTFBSDKLoginButtonView.h
@@ -5,6 +5,8 @@
 
 @interface RCTFBSDKLoginButtonView: UIView<FBSDKLoginButtonDelegate>
 
+@property (nonatomic, strong) FBSDKLoginButton *loginButton;
+
 @property (nonatomic, copy) RCTBubblingEventBlock onChange;
 
 @end

ios/RCTFBSDK/login/RCTFBSDKLoginButtonView.m
@@ -8,8 +8,6 @@
 
 @interface RCTFBSDKLoginButtonView ()
 
-@property (nonatomic, strong) FBSDKLoginButton *loginButton;
-
 @end
 
 @implementation RCTFBSDKLoginButtonView