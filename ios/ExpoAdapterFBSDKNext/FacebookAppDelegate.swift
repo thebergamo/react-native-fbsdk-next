75aa6eddc11aec98332c55df197e36bc674c5935
Lukas <70695015+LukasB-DEV@users.noreply.github.com>
2026-05-05 07:33:34 +0200

    fix(ios): update FBSDK AppDelegate calls for v17 APIs

--- ios/ExpoAdapterFBSDKNext/FacebookAppDelegate.swift
+++ ios/ExpoAdapterFBSDKNext/FacebookAppDelegate.swift
@@ -3,10 +3,7 @@ import FBSDKCoreKit
 
 public class FacebookAppDelegate: ExpoAppDelegateSubscriber {
   public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    ApplicationDelegate.shared.application(
-      application,
-      didFinishLaunchingWithOptions: launchOptions
-    )
+    ApplicationDelegate.shared.initializeSDK()
     return true
   }
 
@@ -14,8 +11,7 @@ public class FacebookAppDelegate: ExpoAppDelegateSubscriber {
     return ApplicationDelegate.shared.application(
       application,
       open: url,
-      sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
-      annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+      options: options
     )
   }
 }
